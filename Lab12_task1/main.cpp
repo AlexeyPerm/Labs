051bba050035f6e64d8034f34312bf116e31d707
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-13 00:24:46 +0500

    upd 12.1

--- Lab12_task1/main.cpp
+++ Lab12_task1/main.cpp
@@ -1,29 +1,75 @@
 #include <iostream>
 #include <random>
+#include <iomanip>
 #include <set>
 
+typedef std::multiset<double, std::less<>> mset;
+
+auto average(mset& ms) -> double;
+auto printMultiset(mset& ms) -> void;
+auto makeMultiset(const int& n) -> mset;
 
 template<class T>
 T generateRandom(const T& left, const T& right);  //рандомайзер
 
-typedef std::set<int, std::less<double>> tset;
-
-
 int main() {
-    constexpr int n = 10;
-    tset s; //создание множества
-    for (int i = 0; i < n; ++i) {
-        constexpr double right = 10.0;
-        s.insert(generateRandom(0.0, right));
-    }
+    constexpr int n = 9;
+//Генерируем 9 элементов множества плюс одно дублируем. На выходе получаем множество с 10 элементами.
+    auto mset = makeMultiset(n);
+    std::cout << "Created stack:\n";
+    printMultiset(mset);
 
-    for (const auto& item: s) {
-        std::cout << item << "  ";
-    }
+    std::cout << "Average: ";
+    auto msetAverage = average(mset);
+    std::cout << msetAverage << std::endl;
+
+//В задании говорится, что нужно поместить в начало контейнера среднее арифметическое.
+//Но так как все элементы множества хранятся отсортированными, то без лишних заморочек
+//добавляем элемент через метод insert().
+    std::cout << "Add average item to the multiset:\n";
+    mset.insert(msetAverage);
+    printMultiset(mset);
+
+
+
 
     return 0;
 }
 
+mset makeMultiset(const int& n) {
+/*Заполняем множество рандомными значениями. Одно из значений обязательно дублируем, так как
+ *это нужно для того, чтобы убедить, что multiset отрабатывает так, как ожидается.
+ *В цикле if() если номер элемента i будет равен сгенерированному числу, а это произойдёт в любому
+ * случае, тогда дублируем элемент, значение которого хранится в переменной x.
+*/
+    mset ms;
+    bool flag = true;
+    for (int i = 0; i < n; ++i) {
+        constexpr double right = 10.0;
+        double x = generateRandom(0.0, right);
+        ms.insert(x);
+        if (i == generateRandom(i, n - 1) && flag) {
+            ms.insert(x);
+            flag = false;
+        }
+    }
+    return ms;
+}
+
+void printMultiset(mset& ms) {
+    for (const auto& item: ms) {
+        std::cout << std::fixed << std::setprecision(2) << item << "  ";
+    }
+    std::cout << std::endl;
+}
+
+double average(mset& ms) {
+    double sum = 0;
+    for (auto& item: ms) {
+        sum += item;
+    }
+    return (sum / ms.size());
+}
 
 template<class T>
 T generateRandom(const T& left, const T& right) {
@@ -45,3 +91,4 @@ T generateRandom(const T& left, const T& right) {
     }
     return 0;
 }
+
