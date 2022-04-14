0cec6a4fafcd72ef3e7c8200214cf7a0744410a9
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-14 22:58:04 +0500

    add lab13.1

---
+++ Lab13_taks1/Functions.h
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <iomanip>
+#include "Money.h"
+
+Money g_m;
+typedef std::vector<Money> Vec;
+auto makeVector(const int& n) -> Vec;
+auto printVector(const Vec& v) -> void;
+
+template<class T>
+T generateRandom(const T& left, const T& right);  //рандомайзер
+
+// --------------------- Predicates ---------------------- //
+
+struct compLess {
+    bool operator()(Money &rhs, Money& lsh) {
+        return rhs > lsh;
+    }
+};
+
+// --------------------- Functions ---------------------- //
+
+Vec makeVector(const int& n) {
+    Vec v;
+    constexpr long lRubles = 0;
+    constexpr int iKopeks = 0;
+    for (int i = 0; i < n; ++i) {
+//Верхняя граница генерирования ограничена, чтобы вывод на экран был более читаем и влез в одну строку.
+        long r = generateRandom(lRubles, lRubles + 1000);
+        int k = generateRandom(iKopeks, iKopeks + 99);
+        Money m(r, k);
+        v.push_back(m);
+    }
+    return v;
+}
+
+void printVector(const Vec& v) {
+    for (const auto& item: v) {
+        std::cout << std::fixed;
+        std::cout << std::setprecision(2);
+        std::cout << item << "  ";
+    }
+    std::cout << std::endl;
+}
+
+template<class T>
+T generateRandom(const T& left, const T& right) {
+    const char* i = typeid(int).name();
+    const char* l = typeid(long).name();
+    const char* d = typeid(double).name();
+    const char* ll = typeid(long long).name();
+    const char* e = typeid(long double).name();
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    if (typeid(T).name() == i || typeid(T).name() == l) {
+        std::uniform_int_distribution<> dis(left, right);
+        return dis(gen);
+    } else if ((typeid(T).name() == d) || (typeid(T).name() == e) || (typeid(T).name() == ll)) {
+        std::uniform_real_distribution<> dis(left, right);
+        return dis(gen);
+    }
+    return 0;
+}
--- Lab13_taks1/main.cpp
+++ Lab13_taks1/main.cpp
@@ -1,6 +1,52 @@
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include "Money.h"
+#include "Functions.h"
 
+typedef std::vector<Money> Vec;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//Генерируем размер вектора от 4 до 10. При необходимости диапазон можно изменить.
+    const int n = generateRandom(4, 10);
+    std::cout << "Generated vector with size = " << n << ":\n";
+    Vec v = makeVector(n);
+    printVector(v);
+    std::cout << "Average: ";
+
+/*std::accumulate() суммирует элементы в контейнере.
+* Последний параметр функции - с какого значения будут производиться
+* вычисления. И он же является возвращаемым типом данных.
+* https://en.cppreference.com/w/cpp/algorithm/accumulate
+ *https://youtu.be/kxYJkyQuYQU
+*/
+    auto averageMoney = std::accumulate(v.begin(), v.end(), g_m) / static_cast<int> (v.size());
+    std::cout << averageMoney << std::endl;
+
+    std::cout << "Add average item to the beginning of the vector:\n";
+    v.insert(v.begin(), averageMoney);
+    printVector(v);
+
+    std::cout << "Duplicate random item:\n";
+    //добавляем дубликат какого-либо элемента в контейнере в случайное место.
+    int randomIndex = generateRandom(0, static_cast<int> (v.size()) - 1);
+    int randomPos = generateRandom(0, static_cast<int> (v.size()) - 1); //случайной номер позиции в контейнере
+    v.insert(v.begin() + randomIndex, v[randomPos]);
+    printVector(v);
+
+    std::cout << "Remove element with value = " << v[randomIndex] << " from the vector. " << std::endl;
+    auto removeElement = v[randomIndex];
+//Можно сразу передать в erase() возвращаемый итератор функции remove(), как стартовую позицию. Сложно читается.
+//v.erase(remove(v.begin(), v.end(), removeElement), v.end());
+//а создать переменную, которая будет хранить возвращаемый итератор.
+    auto it = remove(v.begin(), v.end(), removeElement);
+    v.erase(it, v.end());
+    printVector(v);
+
+    std::cout << "Sorted vector:\n";
+    sort(v.begin(), v.end(), compLess());
+    printVector(v);
+
     return 0;
 }
+
+
