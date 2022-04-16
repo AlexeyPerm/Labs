3a5c7862c08de633433b24315d54c8c9ce311564
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-17 00:26:55 +0500

    unt 13.1

--- untitled13_1/main.cpp
+++ untitled13_1/main.cpp
@@ -1,98 +1,43 @@
-#include <vector>
+#include <map>
 #include <iostream>
+#include <functional>
 #include <algorithm>
-#include "Money.h"
 
-typedef std::vector<Money> Vec;
-Vec makeVector(int n);
-void printVector(Vec& v);
-Money average(Vec& v);
+using namespace std;
 
-
-Money g_m;
-
-struct Greater_s {
-    bool operator()(const Money& m) {
-        return m > g_m;
-    }
-};
-
-struct compLess {
-    bool operator()(const Money& m1, const Money& m2) {
-        return m1 > m2;
-    }
-};
-
-struct equalS {
-    bool operator()(const Money& m) {
-        return m == g_m;
-    }
-
-};
 
 int main() {
-    int n = 3;
-    //std::cin >> n;
-    Vec v;
-    v = makeVector(n);
-    printVector(v);
-    Vec::iterator it;
+    using Map = std::map<int, int>;
+    Map q;
+    Map s;
+    using ggg = pair<int, int>;
+    q.insert(make_pair(3, 2));
+    q.insert(make_pair(1, 3));
+    q.insert(make_pair(2, 3));
+    q.insert(make_pair(4, 1515));
+    vector<pair<int, int>> z;
 
-    it = std::max_element(v.begin(), v.end());
-    std::cout << "Max = " << *it << std::endl;
-    Money x = *it;
-    Money averMoney = average(v);
-    std::cout << "Aver = " << averMoney << std::endl;
+    auto k = pair(2, 1111);
+    auto v = pair(999, 444);
 
-    //replace_if(v.begin(), v.end(), Greater_s(), x);
-    //std::cout << "Change" << std::endl;
-    //printVector(v);
-
-
-    std::cout << "sorted <" << std::endl;
-    sort(v.begin(), v.end(), compLess());
-    printVector(v);
-
-    std::cout << "POISK" << std::endl;
-
-    std::cin>>g_m;
-
-    it = find_if(v.begin(), v.end(), equalS());
-    if (it != v.end()) {
-        std::cout << *(it) << std::endl;
-    } else {
-        std::cout << "Not such element!" << std::endl;
+    for (const auto& i: q) {
+        z.push_back(i);
     }
 
+    for (auto& item: z) {
+        if (item.second == 1515) {
+            item.second = 0;
+        }
+    }
+
+    q.erase(q.begin(), q.end());
+    for (const auto& item: z) {
+        q.insert(item);
+    }
+
+    for (const auto& item: q) {
+        std::cout << item.first << " : " << item.second << endl;
+    }
 
     return 0;
-}
-
-Vec makeVector(const int n) {
-    Money a;
-    Vec v;
-    for (int i = 0; i < n; ++i) {
-        std::cin >> a;
-        v.push_back(a);
-    }
-    return v;
-}
-
-void printVector(Vec& v) {
-    for (const auto& item: v) {
-        std::cout << item << "  ";
-    }
-    std::cout << std::endl;
-}
-
-Money average(Vec& v) {
-    long long sum = 0;
-    for (const auto& item: v) {
-        sum += item.getRubles() * 100 + item.getKopeks();
-    }
-    Money m;
-    const long long tmp = (sum / v.size());
-    m.setRubles(static_cast<int> (tmp / 100));
-    m.setKopeks(static_cast<int> (tmp % 100));
-    return m;
 }
