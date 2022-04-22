3bd3a3fd47a255f702167bff0c3e23a8f0972f89
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-22 12:58:32 +0500

    upd 14

--- Lab14/main.cpp
+++ Lab14/main.cpp
@@ -2,6 +2,14 @@
 #include "Functions.h"
 
 int main() {
-    while (selection());
+    //while (selection());
+
+    auto tree = buildBalancedTree(15);
+    createBalancedSearchTree(tree);
+    tree = insertToTree(tree, 2993);
+    tree = insertToTree(tree, 22);
+    tree = insertToTree(), -10);
+    printTree(tree, 0);
+
     return 0;
 }
