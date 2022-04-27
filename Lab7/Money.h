9005091cea9e2425622486e63d8b1b5726fce4b4
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-27 16:10:02 +0500

    upd lab 7

--- Lab7/Money.h
+++ Lab7/Money.h
@@ -1,28 +1,33 @@
 #pragma once
 
-#include "istream"
-
-using namespace std;
+#include <fstream>
 
 class Money {
 public:
-    Money() = default;
-
+// ------------------------- Constructors -------------------------- //
+    Money() { rubles = 0, kopeks = 0; }
     Money(long r, int k);
-
     Money(const Money&);
-
+    ~Money() = default;
+// ---------------------- Getters / Setters ------------------------ //
+    long getRubles() const { return rubles; }
+    int getKopeks () const { return kopeks; }
+    void setRubles(const long r) { rubles = r; }
+    void setKopeks(const int k)  { kopeks = k; }
+// --------------------- Overloaded Functions ---------------------- //
+    Money& operator=(const Money& rhs);
+    bool operator<(const Money& rhs) const;
+    bool operator>(const Money& rhs) const;
+    Money& operator++();
+    Money  operator++(int);
     Money operator+(const Money&) const;
 
-    Money& operator=(const Money&);
+    friend std::istream& operator>>(std::istream& in, Money& m);
+    friend std::ostream& operator<<(std::ostream& out, const Money& m);
 
-    friend ostream& operator<<(ostream&, const Money&);
-
-    friend istream& operator>>(istream&, Money&);
+    void print() const;
 
 private:
+    int  kopeks;
     long rubles;
-    int kopeks;
 };
-
-
