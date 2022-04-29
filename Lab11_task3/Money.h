6a070787c5e909e4d50c0364dc80249efcec5893
AlexeyPerm <30314481+AlexeyPerm@users.noreply.github.com>
2022-04-29 12:08:05 +0500

    upd 11.3

--- Lab11_task3/Money.h
+++ Lab11_task3/Money.h
@@ -11,23 +11,24 @@ public:
     ~Money() = default;
 // ---------------------- Getters / Setters ------------------------ //
     long getRubles() const { return rubles; }
-    int getKopeks()  const { return kopeks; }
+    int  getKopeks() const { return kopeks; }
     void setRubles(const long r) { rubles = r; }
     void setKopeks(const int  k) { kopeks = k; }
 // --------------------- Overloaded Functions ---------------------- //
-    Money& operator=(const Money& rhs);
+
     Money& operator=(const int& n);   //конвертируем число в объект класса
-    Money operator+(const Money& rhs) const;
+    Money& operator=(const Money& rhs);
     Money& operator+=(const Money& rhs);
-    Money operator-(const Money& rhs) const;
     Money& operator-=(const Money& rhs);
-    Money operator/(const int& n) const;
-    bool operator<(const Money& rhs) const;
+    Money  operator /(const int& n) const;
+    bool   operator<(const Money& rhs) const;
+    Money  operator-(const Money& rhs) const;
+    Money  operator+(const Money& rhs) const;
 
     friend std::istream& operator>>(std::istream&, Money&);
     friend std::ostream& operator<<(std::ostream&, const Money&);
 
 private:
-    int kopeks;
+    int  kopeks;
     long rubles;
 };
